7d5e259ed274c59a74fe17d7353b0bb4eb5f4bc1
Did&K_O <didenko.nazar@list.ru>
2020-10-01 00:00:57 +0700

    settings and runtime

--- sumpractice/settings.py
+++ sumpractice/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
-db_fom_env = dj_database_url.config()
-DATABASES['default'].update(db_fom_env)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -88,7 +85,8 @@ DATABASES = {
     }
 }
 
-
+db_fom_env = dj_database_url.config()
+DATABASES['default'].update(db_fom_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
