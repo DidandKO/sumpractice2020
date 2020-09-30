794ad5bc7d4ecf2ee1a6583d9268b4455dd55abb
Did&K_O <didenko.nazar@list.ru>
2020-09-30 23:47:32 +0700

    procfile, requirements.txt, runtime.txt

--- sumpractice/settings.py
+++ sumpractice/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
+db_fom_env = dj_database_url.config()
+DATABASES['default'].update(db_fom_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +29,7 @@ SECRET_KEY = 'k0in@p%@l%i$3$+x%h1^d-6+w=%9w$vsd!-gzjds4!az#scy#8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['*'
                  ]
 
 
@@ -123,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
